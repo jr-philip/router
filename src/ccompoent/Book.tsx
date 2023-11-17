e3ec12108ceebcb38d609c1462f60655cbfc54c8
jr-philip <philipmwaura48@gmail.com>
2023-11-16 19:05:28 -0600

    Router: Different routers

---
+++ src/ccompoent/Book.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Book = () => {
+  return (
+    <div>
+      lets go
+    </div>
+  )
+}
+
+export default Book
---
+++ src/ccompoent/New.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const New = () => {
+  return (
+    <div>
+      <h1>PEOPLE</h1>
+    </div>
+  );
+}
+
+export default New
